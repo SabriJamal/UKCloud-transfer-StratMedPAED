64f88f7073e7dcfe07db8b6b79d3b45d152e4ee1
Sabri Jamal <s.jamal@live.se>
2019-11-23 14:03:25 +0000

    Initial commit of fully tested UKCloud transfer script version 1.0

---
+++ scripts/transfer_script.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+###########
+## Input ##
+###########
+pool=$1
+trial_id=$2
+sample_t=$3 #moldx
+sample_b=$4 #moldx
+
+######################
+## Static variables ##
+######################
+data_report_path="/scratch/DMP/DUDMP/TRANSGEN/transgen-mdx/ngs/001.reports"
+data_analysis_path="/scratch/DMP/DUDMP/TRANSGEN/transgen-mdx/ngs/3.analysis"
+data_fastq_path="/scratch/DMP/DUDMP/TRANSGEN/transgen-mdx/ngs/2.fastq"
+destination_path="/scratch/DMP/DUDMP/TRANSGEN/transgen-mdx/ngs/UKCloud"
+transfer_log_file="samples_ready_to_transfer.log"
+transfer_log_file="$destination_path/$transfer_log_file"
+suffix="G"
+
+#Compounded vars
+full_sample_name="$sample_t-$trial_id-T"
+destination_path="$destination_path/$full_sample_name"
+pool_germ="$pool$suffix"
+
+##Create destination folder
+mkdir $destination_path
+
+###################
+## Data transfer ##
+###################
+
+#Fastqs
+mkdir -p $destination_path/Fastqs
+cp $data_fastq_path/$pool/$full_sample_name*fastq.gz $destination_path/Fastqs/.
+
+#Alignments
+mkdir $destination_path/Alignments
+cp $data_report_path/$pool/Alignments/*$trial_id* $destination_path/Alignments/.
+
+#CNVs
+mkdir -p $destination_path/CNVs/CNV_V2
+mkdir -p $destination_path/CNVs/CNVkit
+cp -r $data_analysis_path/$pool/CNVs/CNV_V2/*$trial_id*/* $destination_path/CNVs/CNV_V2/.
+cp -r $data_analysis_path/$pool/CNVs/CNVkit/*$trial_id*/* $destination_path/CNVs/CNVkit/.
+
+#Reports
+mkdir -p $destination_path/Reports/Germline
+mkdir -p $destination_path/Reports/Somatic
+cp $data_report_path/$pool/Variants/$sample_b-$trial_id-B/$sample_t-$trial_id-T.mutation.report $destination_path/Reports/Somatic/.
+cp $data_report_path/$pool_germ/Variants/$sample_b-$trial_id-B/$sample_b-$trial_id-B.mutation.report $destination_path/Reports/Germline/.
+cp $data_report_path/$pool/Reports/$sample_t-$trial_id-T.patient.report $destination_path/Reports/Somatic/.
+cp $data_report_path/$pool_germ/Reports/$sample_b-$trial_id-B.patient.report $destination_path/Reports/Germline/.
+cp $data_report_path/$pool/SVs/$sample_b-$trial_id-B/$sample_t-$trial_id-T.SV.report $destination_path/Reports/Somatic/.
+cp $data_report_path/$pool/CNVs/$sample_t-$trial_id-T.cnv.report $destination_path/Reports/Somatic/.
+
+#SVs 
+mkdir -p $destination_path/SVs/Manta
+cp -r $data_analysis_path/$pool/SVs/AnnotSV/*$trial_id*-T/* $destination_path/SVs/Manta/.
+
+##Remove intermediary SV files
+rm $destination_path/SVs/Manta/*[wW]orkflow*
+rm -rf $destination_path/SVs/Manta/workspace
+
+#Stats
+mkdir $destination_path/Stats
+cp $data_report_path/$pool/Stats/*$trial_id* $destination_path/Stats/.
+
+#Variants (Raw VCF)
+mkdir -p $destination_path/Variants/Germline
+mkdir -p $destination_path/Variants/Somatic
+cp $data_report_path/$pool/Variants/$sample_b-$trial_id-B/SNV.$sample_t-$trial_id-T.vcf $destination_path/Variants/Somatic/.
+cp $data_report_path/$pool_germ/Variants/$sample_b-$trial_id-B/GATK*$trial_id-B.vcf $destination_path/Variants/Germline/.
+
+##Send files to UKCloud
+module load anaconda/3/4.4.0
+source activate UKcloud
+
+#Send sample data to UKCloud
+if [[ -d $destination_path ]];
+then
+	printf "\nTransferring $destination_path to UKCloud...\n"
+	s3cmd put $destination_path --recursive s3://smpaeds/CMP/
+	rm -rf $destination_path
+	printf "\nTransfer succesful, deleting local copy $destination_path\n"
+fi
+
+#Send transfer log data to UKCloud
+if [[ -f $transfer_log_file ]];
+then
+	printf "\nTransferring $transfer_log_file to UKCloud\n"
+	s3cmd put $transfer_log_file s3://smpaeds/CMP/
+fi
